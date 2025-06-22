28d0cfa7a182abf491bb42cb75c628d44e728e50
Prabhat Kumar Tiwari <61203736+Prabhat-kr-Tiwari@users.noreply.github.com>
2025-06-22 23:20:54 +0530

    content provider

---
+++ app/src/main/java/com/prabhat/activitylifecycle/ImageViewModel.kt
@@ -0,0 +1,20 @@
+package com.prabhat.activitylifecycle
+
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class ImageViewModel : ViewModel() {
+
+
+    var images by mutableStateOf(emptyList<Image>())
+        private set
+
+    fun updateImage(images: List<Image>) {
+        this.images = images
+    }
+
+
+}
--- app/src/main/java/com/prabhat/activitylifecycle/MainActivity.kt
+++ app/src/main/java/com/prabhat/activitylifecycle/MainActivity.kt
@@ -1,35 +1,102 @@
 package com.prabhat.activitylifecycle
 
+import android.Manifest
+import android.content.ContentUris
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import coil3.compose.AsyncImage
 import com.prabhat.activitylifecycle.ui.theme.ActivityLifecycleTheme
+import java.util.Calendar
 
 class MainActivity : ComponentActivity() {
+
+    val imageviewmodel by viewModels<ImageViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-       val drawbale =  resources.getDrawable(R.drawable.main_before,null)
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_MEDIA_IMAGES), 0)
+        val drawbale = resources.getDrawable(R.drawable.main_before, null)
+        val projection = arrayOf(
+            MediaStore.Images.Media._ID,
+            MediaStore.Images.Media.DISPLAY_NAME
+        )
+        val milisYesterday = Calendar.getInstance().apply {
+            add(Calendar.DAY_OF_YEAR, -1)
+        }.timeInMillis
+        val selection = "${MediaStore.Images.Media.DATE_TAKEN}>=?"
+        val selectionArgs = arrayOf(milisYesterday.toString())
+        val sordOrder = "${MediaStore.Images.Media.DATE_TAKEN} DESC"
+        contentResolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            projection,
+            selection,
+            selectionArgs,
+            sordOrder
+        )?.use { cursor ->
+            val idColumn = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+            val nameColumn = cursor.getColumnIndex(MediaStore.Images.Media.DISPLAY_NAME)
+            val image = mutableListOf<Image>()
+            while (cursor.moveToNext()) {
+
+                val id = cursor.getLong(idColumn)
+                val name = cursor.getString(nameColumn)
+
+                val uri = ContentUris.withAppendedId(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id
+                )
+                image.add(Image(id, name, uri))
+
+
+            }
+            imageviewmodel.updateImage(image)
+
+        }
         setContent {
             ActivityLifecycleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+
+                LazyColumn(modifier = Modifier.fillMaxSize()) {
+                    items(imageviewmodel.images.size) { index ->
+                        Column(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            AsyncImage(
+                                model = imageviewmodel.images[index].uri,
+                                contentDescription = null
+                            )
+                            Text(imageviewmodel.images[index].name)
+                        }
+                    }
                 }
+
+
             }
         }
     }
 }
+
+
+data class Image(
+    val id: Long,
+    val name: String,
+    val uri: Uri
+)
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
